7ce13e6343c59a361ab9214103de5d9a97099b5f
ezzeldinAI <ezzeldin.coder.ai@gmail.com>
2025-06-12 10:23:38 +0400

    Revert ":sparkles: setup eslint / lint-staged"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-05-15",
+  compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  eslint: {
-    config: {
-      standalone: false,
-      autoInit: false,
-    },
-  },
+
   modules: [
-    "@nuxt/eslint",
-    "@nuxt/ui",
-    "@nuxt/icon",
-    "@nuxt/fonts",
-  ],
-});
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/icon',
+    '@nuxt/fonts'
+  ]
+})
