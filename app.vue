4647f34f2327ddf485eefeed3dcfe7ff0b9dc487
ezzeldinAI <ezzeldin.coder.ai@gmail.com>
2025-06-12 13:58:37 +0400

    :sparkles: setup tailwind / daisy ui / icons

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <button class="btn btn-success btn-soft btn-xs">
+      View Map Location <Icon name="tabler:map-pin-filled" />
+    </button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
     '@nuxt/icon',
     '@nuxt/fonts'
-  ]
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
 })
